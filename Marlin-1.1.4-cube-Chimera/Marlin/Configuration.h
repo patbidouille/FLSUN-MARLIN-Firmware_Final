6d604b07f5d1a4507490c7076465046a92645acc
patbidouille <patbidouille@free.fr>
2020-02-06 09:40:34 +0100

    nv-060220

--- Marlin-1.1.4-cube-Chimera/Marlin/Configuration.h
+++ Marlin-1.1.4-cube-Chimera/Marlin/Configuration.h
@@ -1017,6 +1017,8 @@
 // Homing speeds (mm/m)
 //#define HOMING_FEEDRATE_XY (40*60)
 //#define HOMING_FEEDRATE_Z  (55)
+#define HOMING_FEEDRATE_XY (50*60)
+#define HOMING_FEEDRATE_Z  (4*60)
 
 //=============================================================================
 //============================= Additional Features ===========================
